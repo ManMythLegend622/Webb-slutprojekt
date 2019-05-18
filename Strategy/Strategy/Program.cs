4164dc0cbb77a76a61d8662ce4a73617d576466e
Robin Johansson <rojo01001@utb.vaxjo.se>
2019-05-18 20:53:47 +0200

    Kommentarer och placeholder text.

--- Strategy/Strategy/Program.cs
+++ Strategy/Strategy/Program.cs
@@ -6,15 +6,26 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Strategy</title>
         <link rel = "stylesheet" type="text/css" href="CssSpel.css"> <!--Referar till CSS filen.-->
+        <style>
+
+              div {
+    font - size: 2rem;
+}
+        </style>
     </head>
     <body bgcolor = "Blue" >
-  
-          < div class="row">
-            <div class="column"></div>
-            <div class="column"></div>
+        < P > < a href="webb.html">Tillbaka till hubben</a></P>
+        <div class="row">
+            <div class="column"> <!--De tre spalterna-->
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="column">
+                <h1>Fakta om strategispel</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
             <div class="column">
                 <p> Vilken är din favorit typ av strategi spel och vilket är det?</p>
-                <select>
+                <select> <!--Forms frågorna-->
                     <optgroup label = "4X" >
                         < option value="civilization">Civlization</option>
                         <option value = "galactic civiliations" > Galactic Civilizations</option>
