afabb2be5c5d632f55e18521662ab503f28c36b8
Robin Johansson <rojo01001@utb.vaxjo.se>
2019-05-18 18:18:28 +0200

    Fyllde ut css sidan med egenskaperna för columner samt lite för tabellernas färger. La även till komentarer och delade upp CSS sidan i bitar så att det blir mer organiserat.

--- CssSpel/CssSpel/Program.cs
+++ CssSpel/CssSpel/Program.cs
@@ -1,3 +1,6 @@
+
+/*------------------------------------Nav menyn--------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
 nav > li > a { /*Alla a som child till li som child till nav displayas som ett block i mitten av skärmen*/
     display: block;
     text-decoration: none;
@@ -5,11 +8,11 @@ nav > li > a { /*Alla a som child till li som child till nav displayas som ett b
 }
 
 li a:hover {
-    background-color: rgb(60, 200, 160); 
+    background-color: rgb(60, 200, 160); /*Blir grönaktig färg när musen är över knappen*/
 }
 
 li a:active {
-    background-color: rgb(255,0,0); 
+    background-color: rgb(255,0,0); /*Blir till röd färg när knappen trycks*/
 }
 
 li a
@@ -17,9 +20,15 @@ li a
     list-style: none;
     margin: 0;
     display: block;
-    padding: 16px 90px;
+    padding: 16px 80px;
     color: white;
     text-decoration: none;
+}
+@media screen and(max-width: 900px)
+{ /*Om skärmen är mindre än 900 pixlar knapparna vara en per rad istället*/
+    li a {
+    padding: 16px 350px;
+    }
 }
 
 li {
@@ -28,9 +37,9 @@ li {
 }
 
 
-    li:last.child {
-        border-right: none;
-    }
+li:last.child {
+    border-right: none;
+}
 
 ul {
     border-radius: 32px;
@@ -42,3 +51,34 @@ ul {
     background-color: rgb(60, 20, 200);
 }
 
+/*-------------------------Slut på kod för nav meny, början på kod för columnerna--------------------------------------------------------------------------------------------------------------*/
+
+.column { /*Skapar columner som är lika stora och täcker en tredjedel av skärmen*/
+    float: left;
+    width: 33.33%;
+    background-color: hsla(0, 100%, 100%, 0.8);
+}
+
+.row:after {
+    content: "";
+    display: table;
+    clear: both;
+}
+
+
+@media screen and(max-width: 600px)
+{ /*Om skärmen är mindre än 600 pixlar så kommer columnerna åvanför varandra istället*/
+  .column {
+    width: 100 %;
+    }
+}
+
+/*-------------------------Slut på kod för columnerna----------------------------------------------------------------------------------------------------------------------------------------------*/
+
+thead {color:green;}
+tbody {color:blue;}
+tfoot {color:red;}
+
+caption {
+    font-size: 32px;
+}
