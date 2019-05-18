6efd46f9db5aa00f4542766e22c3e8cde791dfe2
Robin Johansson <rojo01001@utb.vaxjo.se>
2019-05-18 20:54:56 +0200

    Iframe länk, osoreterade listor, kommentarer och placeholdertext.

--- Adventure/Adventure/Program.cs
+++ Adventure/Adventure/Program.cs
@@ -7,6 +7,33 @@
         <title>Adventure</title>
         <link rel = "stylesheet" type="text/css" href="CssSpel.css"> <!--Referar till CSS filen.-->
     </head>
-    <body>
+    <body bgcolor = "Green" >
+  
+          < P > < a href= "webb.html" > Tillbaka till hubben</a></P>
+        <div class="row">
+            <div class="column"> <!--Tre columner-->
+                <p>olika äventyrsspel</p>
+                <ol>        <!--osorterad lista-->
+                    <li>Firewatch</li>
+                    <li>Spore</li>
+                    <li>Life is strange</li>
+                    <li>Minecraft</li>
+                    <li>Gone Home</li>
+                </ol>
+            </div>
+            <div class="column">
+                <ol start = "6" >
+                    < li > Soma </ li >
+                    < li > Monkey Island</li>
+                    <li>Her Story</li>
+                    <li>Night in the Woods</li>
+                    <li>The Walking Dead</li>
+                </ol>
+            </div>
+            <div class="column">
+                <p>Lista med några adventure spel du är intresserad.</p>
+                <iframe src = https://www.youtube.com/embed/F2DR7-8jFC0></iframe> <!--iframe klippet-->
+            </div>
+        </div>
     </body>
 </html>
