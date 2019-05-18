2a812112b94a023bd7823a3bfd104d84c3109c4c
Robin Johansson <rojo01001@utb.vaxjo.se>
2019-05-18 20:51:16 +0200

    La in kommentarer och placholder text (lorem ipsum).

--- Webb/Webb/Program.cs
+++ Webb/Webb/Program.cs
@@ -30,21 +30,23 @@
           </ nav >
   
           < div class="row">
-            <div class="column">
-                <p>123</p>
+            <div class="column"> <!--De tre spalterna-->
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
             <div class="column">
-                <p>123</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
             <div class="column">
                 <dl>
-                    <dt>Action</dt>
-                    <dd>123</dd>
+                    <dt>Action</dt> <!--Lista och beskrivning för de olika genrerna-->
+                    <dd>Actionspel är en abstrakt datorspelsgenre som fokuserar på fysiska utmaningar, däribland reaktionstid och koordination.</dd>
                     <dt>Adventure</dt>
-                    <dd>123</dd>
+                    <dd>Äventyrsspel är en övergripande genre inom dator- och TV-spel som kännetecknas av ett starkt historieberättande inslag.</dd>
+                    <dt>Strategy</dt>
+                    <dd>Strategispel är ett spel där deltagarna använder sitt strategiska tänkande.</dd>
                 </dl>
-                <p>Vad tycker du om denna sida från 1 till 100?</p>
-                <p>Dra slidern till vänster eller skriv in ett tal till höger...eller båda.</p>
+                <p>Vad tycker du om denna sida från 1 till 100?</p> <!--Forms frågor-->
+                <p>Dra slidern till vänster eller skriv in ett tal till höger... eller båda.</p>
                 <form oninput = "x.value=parseInt(a.value)+parseInt(b.value)" > 0
                     < input type= "range" id= "a" value= "50" > 100
                     +< input type= "number" id= "b" value= "50" >
@@ -52,8 +54,8 @@
                 </form>
             </div>
         </div>
+        <footer>
+            <p>Projekt som ska vara inne<time datetime="2019-05-19">19:de maj.</time></p> <!--text med time ooch datetime värden-->
+        </footer>
     </body>
-    <footer>
-        <p>Projekt som ska vara inne<time datetime="2019-05-19">19:de maj.</time></p>
-    </footer>
 </html>
