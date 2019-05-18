3ddd5e7896318c8ed87e2fd2353808f261477f6c
Robin Johansson <rojo01001@utb.vaxjo.se>
2019-05-18 18:22:54 +0200

    Lade till strespalters columnerna och lade även in listan med thead, tbody och tfoot.

--- Action/Action/Program.cs
+++ Action/Action/Program.cs
@@ -7,6 +7,47 @@
         <title>Action</title>
         <link rel = "stylesheet" type="text/css" href="CssSpel.css"> <!--Referar till CSS filen.-->
     </head>
-    <body>
+    <body bgcolor = "Red" >
+  
+          < div class="row">
+            <div class="column">
+                <table>
+                    <caption>Populära action spelserier och mängden spel i serierna</caption>
+                    <thead>
+                        <tr>
+                            <th>Populära spelserier med denna genre</th>
+                            <th>Antal spel i huvudserie</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>GTA</td>
+                            <td>6</td>
+                        </tr>
+                        <tr>
+                            <td>Assassins Creed</td>
+                            <td>12</td>
+                        </tr>
+                        <tr>
+                            <td>God of War</td>
+                            <td>4</td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td>Mängd spel</td>
+                            <td>22</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+            <div class="column">
+                <h1>Vad är ett action spel?</h1>
+                <p>1231142</p>
+            </div>
+            <div class="column">
+                
+            </div>
+        </div>
     </body>
 </html>
