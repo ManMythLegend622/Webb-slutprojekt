e6c9a068f584cf035e23850651e17936076d50ee
Robin Johansson <rojo01001@utb.vaxjo.se>
2019-05-18 18:21:10 +0200

    Gjorde den till trespalters. Lade till en form på höger sida. Satte även in en time och datetime i footern med när den ska vara inne.

--- Webb/Webb/Program.cs
+++ Webb/Webb/Program.cs
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html lang = "en" >
+
+
     < head >
         < meta charset="UTF-8">
         <meta name = "viewport" content="width=device-width, initial-scale=1.0">
@@ -10,17 +12,48 @@
     <body>
         <nav>
             <ul>
-                <li><a href = "Action.html" > Action </ a ></ li >
-                  < li >< a href= "" > Adventure </ a ></ li >
-                  < li >< a href= "" > Strategy </ a ></ li >
-                  < li >< a href= "" > Sport </ a ></ li >
-                  < li >< a href= "" > RPG </ a ></ li >
-                  < li >< a href= "" > Fighting </ a ></ li >
-                  < li >< a href= "" > Simulation </ a ></ li >
+                <li><a href = "Action.html" > Action </ a ></ li >  < !--länkar till de olika genrerna-->
+                <li><a href = "Adventure.html" > Adventure </ a ></ li >
+  
+                  < li >< a href= "Strategy.html" > Strategy </ a ></ li >
+  
+                  < li >< a href= "Sport.html" > Sport </ a ></ li >
+  
+                  < li >< a href= "RPG.html" > RPG </ a ></ li >
+  
+                  < li >< a href= "Fighting.html" > Fighting </ a ></ li >
+  
+                  < li >< a href= "Simulation.html" > Simulation </ a ></ li >
   
               </ ul >
   
           </ nav >
   
-      </ body >
-  </ html >
+          < div class="row">
+            <div class="column">
+                <p>123</p>
+            </div>
+            <div class="column">
+                <p>123</p>
+            </div>
+            <div class="column">
+                <dl>
+                    <dt>Action</dt>
+                    <dd>123</dd>
+                    <dt>Adventure</dt>
+                    <dd>123</dd>
+                </dl>
+                <p>Vad tycker du om denna sida från 1 till 100?</p>
+                <p>Dra slidern till vänster eller skriv in ett tal till höger...eller båda.</p>
+                <form oninput = "x.value=parseInt(a.value)+parseInt(b.value)" > 0
+                    < input type= "range" id= "a" value= "50" > 100
+                    +< input type= "number" id= "b" value= "50" >
+                    =< output name= "x" for="a b"></output>
+                </form>
+            </div>
+        </div>
+    </body>
+    <footer>
+        <p>Projekt som ska vara inne<time datetime="2019-05-19">19:de maj.</time></p>
+    </footer>
+</html>
